da4fd7c99a11f1ce30f8543fa3968ee330a27cd7
TheChickenIsOnFire <jmoore@dolgeville.org>
2025-04-17 12:17:53 +0000

    New files

--- background.js
+++ background.js
@@ -1,11 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
-    id: 'main',
-    innerBounds: {
-      width: 800,
-      height: 600,
-      minWidth: 400,
-      minHeight: 400
+    'outerBounds': {
+      'width': 800,
+      'height': 600
     }
   });
 });
