020059a2c03926314005fe0d63b7f790ff8dd13e
TheChickenIsOnFire <jmoore@dolgeville.org>
2025-04-17 12:19:33 +0000

    Fixes

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
-    'outerBounds': {
+    'bounds': {
       'width': 800,
       'height': 600
     }
